04debc8c2a6e23b2338b19e5486ae371ea95c193
EuthimisKexa <117942568+EuthimisKexa@users.noreply.github.com>
2025-10-22 03:09:04 +0000

    checkes complete

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ const errorLabPassword = document.getElementById('errorLabPassword');
 form.addEventListener('submit', function(event) {
 
     let isValid = true;
-
+    
+    //are the inputs empty?
     if (usernameInput.value.trim() === '') {
         errorLabUser.innerHTML = 'Username is <i>required</i>.';
         isValid = false;
@@ -29,18 +30,38 @@ form.addEventListener('submit', function(event) {
         errorLabEmail.hidden = true;
     }
 
+
+
+    //are the inputs correct?
+
+    const emailRegex =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[A-Za-z]{2,}$/;
+    if (!emailInput.value.match(emailRegex)) {
+        errorLabEmail.innerHTML = 'Please enter a <i>valid</i> email address.';
+        isValid = false;
+        errorLabEmail.hidden = false;
+    } else {
+        errorLabEmail.hidden = true;
+    }
+    const passwordRegex = /^(?=.{8,}$)(?=.*[!@#$%^&*()_\-]).*$/;
     if (passwordInput.value.trim() === '') {
-        errorLabPassword.innerHTML = 'Password is <i>required</i>.';
+        errorLabPassword.innerHTML = 'Password must be at least 8 characters and include at least one special character like: !@#$%^&*()_ .';
         isValid = false;
         errorLabPassword.hidden = false;
     }else {
         errorLabPassword.hidden = true;
     }
 
-    //error display elements
     
 
     if (!isValid) {
         event.preventDefault();
+    }else {
+        alert('Form submitted successfully!');
+        console.log('Username:', usernameInput.value);
+        console.log('Email:', emailInput.value); 
+        console.log('Password:', passwordInput.value);
+        window.location.href = newUrl
     }
+
+
     });
