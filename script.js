1f1a876dc23716e780939dfc2b4cca04c59cbd67
EuthimisKexa <117942568+EuthimisKexa@users.noreply.github.com>
2025-10-22 02:40:03 +0000

    add error labels for form

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+//get form and input elements
 const form = document.getElementById('signupForm');
 const usernameInput = document.getElementById('Username');
 const emailInput = document.getElementById('Email');
 const passwordInput = document.getElementById('Password');
 
+const errorLabUser = document.getElementById('errorLabUser');
+const errorLabEmail = document.getElementById('errorLabEmail');
+const errorLabPassword = document.getElementById('errorLabPassword');
+
+//add event listener for form submission
 form.addEventListener('submit', function(event) {
-    console.log('Form submitted');
-    console.log(usernameInput.value);
-    console.log(emailInput.value);
-    console.log(passwordInput.value);
+
+    let isValid = false;
+
+    //error display elements
+
+    errorLabEmail.hidden = false;
+    errorLabUser.hidden = false;
+    errorLabPassword.hidden = false;
+    
+
+    if (!isValid) {
+    event.preventDefault();
+    }
     });
